c15c9aacec2e1d658c391e9626a0a8561ec05c75
ohsssuk <ohsssuk@naver.com>
2025-01-31 18:10:01 +0900

    리뷰 삭제 기능, 병렬 라우트, 인터셉팅 라우트 적용

---
+++ src/app/parallel/layout.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function Layout({
+  children,
+  sidebar,
+  feed,
+}: {
+  children: ReactNode;
+  sidebar: ReactNode;
+  feed: ReactNode;
+}) {
+  return (
+    <div>
+      <div>
+        <Link href={"/parallel"}>Parallel</Link>
+        &nbsp;
+        <Link href={"/parallel/setting"}>Parallel/setting</Link>
+      </div>
+      <br />
+      {children}
+      {sidebar}
+      {feed}
+    </div>
+  );
+}
